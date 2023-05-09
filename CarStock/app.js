8e33114a2ed73c4416648df83d7edf81689a12c7
Piotr Kaczmarek <bobil111@wp.pl>
2023-05-09 13:54:54 +0200

    working_server

--- CarStock/app.js
+++ CarStock/app.js
@@ -2,26 +2,24 @@ const express =  require('express');
 const app = express();
 // (fs) służy do obsługi operacji plikowych 
 const fs = require('fs');
-app.use(express.static(__dirname));//funkcja konczy serwerowania plikow
-
-app.get('/:id', (req, res) => {
+app.get('/vehicle/:id', (req, res) => {
     const carsData = fs.readFileSync('./data.json');
-    console.log("siema")
     const cars = JSON.parse(carsData);
     const car = cars.find(car => car.id == req.params.id); //   pozwala na pobieranie wartości z parametrów ścieżki URL (id) przez funkcje parms
+    res.send({
+        id: car.id,
+        model: car.model,
+        modelYear: car.model,
+        power: car.power,
+        color: car.red,
+        img: car.img
+      });
+});
 
-    if (!car) {
-        res.send('Samochód nie został znaleziony'); // zwraca błąd gdy samochód nie został znaleziony
-    } else {
-        const car_id = document.querySelector("mid-box1");
-        const car_model = document.querySelector("mid-box2");
-        const car_modelYear = document.querySelector("mid-box3");
-        const car_power = document.querySelector("mid-box4");
-        const car_color = document.querySelector("mid-box5");
-       // res.send(print_car=car.id); // wyświetla informacje o samochodzie
-         res.send(car_id=car.id);
-         //res.send(`car_id: ${car.model}, rok produkcji: ${car.modelYear}, moc: ${car.power}, kolor: ${car.color}`);
-    }
+app.get('/allVehicles', (req, res) => {
+    const carsData = fs.readFileSync('./data.json');
+    const cars = JSON.parse(carsData);
+    res.send(cars);
 });
 
 app.listen(3000, () => {
