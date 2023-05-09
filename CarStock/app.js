6258d2747fa34df5631f2f3b1b68f4508f9e04db
Piotr Kaczmarek <bobil111@wp.pl>
2023-05-09 09:39:35 +0200

    initial_server

--- CarStock/app.js
+++ CarStock/app.js
@@ -2,25 +2,24 @@ const express =  require('express');
 const app = express();
 // (fs) służy do obsługi operacji plikowych 
 const fs = require('fs');
+app.use(express.static(__dirname));//funkcja konczy serwerowania plikow
 
-
-app.get('/allStock/:id', (req, res) => {
+app.get('/index.html/:id', (req, res) => {
     const carsData = fs.readFileSync('./data.json');
+    console.log("siema")
     const cars = JSON.parse(carsData);
-
     const car = cars.find(car => car.id == req.params.id); //   pozwala na pobieranie wartości z parametrów ścieżki URL (id) przez funkcje parms
 
     if (!car) {
         res.send('Samochód nie został znaleziony'); // zwraca błąd gdy samochód nie został znaleziony
     } else {
-        res.send(`Model: ${car.model}, rok produkcji: ${car.modelYear}, moc: ${car.power}, kolor: ${car.color}`); // wyświetla informacje o samochodzie
+        let car_id = document.querySelector("mid-box1");
+       // res.send(print_car=car.id); // wyświetla informacje o samochodzie
+         res.send(`Model: ${car.model}, rok produkcji: ${car.modelYear}, moc: ${car.power}, kolor: ${car.color}`);
     }
-});
 
+
+});
 app.listen(3000, () => {
-    console.log("serwer dziala na porcie 3000");
+    console.log("serwer dziala na procie 3000");
 });
-
-
-
-
