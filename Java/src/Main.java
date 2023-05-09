64eaaf5793c3636aecc80513296946c3e800355d
jakubwislaWTT <jakubwisla@wundermanthompson.com>
2023-05-09 13:07:55 +0200

    .

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -11,18 +11,16 @@ public class Siema
 {
     public static void main(String[] args)
     {
-        //JSON parser object to parse read file
+
         JSONParser jsonParser = new JSONParser();
 
         try (FileReader reader = new FileReader("employees.json"))
         {
-            //Read JSON file
             Object obj = jsonParser.parse(reader);
 
             JSONArray employeeList = (JSONArray) obj;
             System.out.println(employeeList);
 
-            //Iterate over employee array
             employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
 
         } catch (FileNotFoundException e) {
@@ -36,18 +34,15 @@ public class Siema
 
     private static void parseEmployeeObject(JSONObject employee)
     {
-        //Get employee object within list
+
         JSONObject employeeObject = (JSONObject) employee.get("employee");
 
-        //Get employee first name
         String firstName = (String) employeeObject.get("firstName");
         System.out.println(firstName);
 
-        //Get employee last name
         String lastName = (String) employeeObject.get("lastName");
         System.out.println(lastName);
 
-        //Get employee website name
         String website = (String) employeeObject.get("website");
         System.out.println(website);
     }
