daa8291821bd4d49b6b34b6f2eb15d82998cd821
Jacek Jedra <jacenty2006@gmail.com>
2023-05-12 12:15:03 +0200

    using gson

--- CarPricer/src/main/java/com/example/springboot/Pricer.java
+++ CarPricer/src/main/java/com/example/springboot/Pricer.java
@@ -3,71 +3,70 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.io.*;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URI;
 import java.util.Objects;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.google.gson.*;
 
 @RestController
 public class Pricer {
 
+	@Autowired
+	private Gson gson;
+
+
 	@GetMapping("/priceFor/{id}")
-	// return JSON
-	public JSONObject price(@PathVariable("id") String carId) throws URISyntaxException,  MalformedURLException{
-		JSONParser parser = new JSONParser();
+	public JsonObject price(@PathVariable("id") String carId) throws MalformedURLException, URISyntaxException {
 
-		// 1. create new endpoint to return JSON data with id, price and currency
-		// 2. this endpoint should take Piotr's endpoint and the endpoint from point 1 and should merge the data for requested id
 		URL url = new URI("http://127.0.0.1:3000/allVehicles").toURL();
-		URL url2 = new URI("http://127.0.0.1:8000/price").toURL();
 
+		try {
 			try (InputStream input = url.openStream()) {
 				InputStreamReader isr = new InputStreamReader(input);
 				BufferedReader read = new BufferedReader(isr);
-				JSONArray arr = (JSONArray) parser.parse(read);
+				JsonArray arr = (JsonArray) JsonParser.parseReader(read);
 
+
+				URL url2 = new URI("http://127.0.0.1:8000/price").toURL();
 				InputStream input2 = url2.openStream();
 				InputStreamReader isr2 = new InputStreamReader(input2);
 				BufferedReader read2 = new BufferedReader(isr2);
-				JSONArray arr2 = (JSONArray) parser.parse(read2);
+				JsonArray arr2 = (JsonArray) JsonParser.parseReader(read2);
+
 
 				for (Object o : arr) {
 
-					JSONObject car = (JSONObject) o;
+					JsonObject car = (JsonObject) o;
 
-					String id = (String) car.get("id");
+					JsonElement id = car.get("id");
+					if(Objects.equals(gson.fromJson(carId, JsonElement.class), id)) {
 
-					for (Object p : arr2){
-						JSONObject pr = (JSONObject) p;
+						for(Object a : arr2){
+							JsonObject pricer = (JsonObject) a;
 
-						String id2 = (String) pr.get("id");
+							JsonElement id2 = pricer.get("id");
 
-						long price = (long) pr.get("price");
+							if(Objects.equals(id, id2)){
+								JsonElement price = pricer.get("price");
 
-						String currency = (String)pr.get("currency");
+								JsonElement currency = pricer.get("currency");
 
-						if(Objects.equals(id, id2)){
-							car.put("price", price);
-							car.put("currency",currency);
+								car.add("price",price);
+								car.add("currency",currency);
+							}
 						}
-
-					}
-
-					if(Objects.equals(carId, id)){
 						return car;
 					}
-
 				}
-			} catch (IOException | ParseException e) {
-				e.printStackTrace();
 			}
-		return new JSONObject();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		return new JsonObject();
 	}
 
 }
