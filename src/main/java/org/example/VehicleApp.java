5314a992ad7e410f25d13c2a73a6fbbb05eab144
jakubwislaWTT <jakubwisla@wundermanthompson.com>
2023-05-16 09:26:26 +0200

    1.3

--- src/main/java/org/example/VehicleApp.java
+++ src/main/java/org/example/VehicleApp.java
@@ -8,15 +8,17 @@ import java.util.*;
 import java.util.stream.Collectors;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import java.io.IOException;
 
 public class VehicleApp {
     private static final String API_URL = "http://localhost:3000/allVehicles";
+    private static final String API_PRICES_URL = "http://localhost:8000/price";
 
     public static void main(String[] args) throws Exception {
-
-        String json = getJSONFromAPI();
-
+        String json = getJSONFromAPI(API_URL);
         List<Vehicle> vehicles = new Gson().fromJson(json, new TypeToken<List<Vehicle>>() {}.getType());
+
+        assignPricesToVehicles(vehicles);
 
         Optional<Vehicle> mostPowerfulVehicle = vehicles.stream()
                 .max(Comparator.comparingInt(Vehicle::getPower));
@@ -35,11 +37,27 @@ public class VehicleApp {
 
         System.out.print("\n");
 
-        vehicles.forEach(vehicle -> System.out.println(vehicle.getModel() + " - " + vehicle.getPrice()));
+        vehicles.forEach(vehicle -> System.out.println(vehicle.getModel() + " - " + String.format("%.2f" , vehicle.getPrice())  + " " + vehicle.getCurrency()));
     }
 
-    private static String getJSONFromAPI() throws Exception{
-        URI uri = new URI(API_URL);
+    private static void assignPricesToVehicles(List<Vehicle> vehicles) throws Exception {
+        String json = getJSONFromAPI(API_PRICES_URL);
+        List<Price> prices = new Gson().fromJson(json, new TypeToken<List<Price>>() {}.getType());
+
+        for (Vehicle vehicle : vehicles) {
+            Optional<Price> matchingPrice = prices.stream()
+                    .filter(price -> price.getId() != null && price.getId().equals(vehicle.getId()))
+                    .findFirst();
+
+            if (matchingPrice.isPresent()) {
+                vehicle.setPrice(matchingPrice.get().getPrice());
+                vehicle.setCurrency(matchingPrice.get().getCurrency());
+            }
+        }
+    }
+
+    private static String getJSONFromAPI(String apiUrl) throws Exception {
+        URI uri = new URI(apiUrl);
         HttpURLConnection con = (HttpURLConnection) uri.toURL().openConnection();
         con.setRequestMethod("GET");
 
@@ -48,16 +66,17 @@ public class VehicleApp {
             throw new RuntimeException("Błąd: " + responseCode);
         }
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
         StringBuilder content = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Błąd odczytu danych z API", e);
+        } finally {
+            con.disconnect();
         }
-        in.close();
-        con.disconnect();
-
         return content.toString();
     }
-
 }
